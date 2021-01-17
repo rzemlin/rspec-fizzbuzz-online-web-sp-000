ea3340ba1442b0a4a381bca6368e9eb28f4b360d
Ryan Zemlin <rzemlin@gmail.com>
2021-01-17 22:44:05 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,9 +3,9 @@
 def fizzbuzz(num)
   if num % 3 == 0 
     "Fizz"
-  if num % 5 == 0 
+  elsif num % 5 == 0 
     "Buzz"
-  if num % 3 == 0 && % 5 == 0
+  elsif num % 3 == 0 && % 5 == 0
     "FizzBuzz"
   end
 end
