88da26c5c01c80fe03b5d7c2e2072f0285901312
Ryan Zemlin <rzemlin@gmail.com>
2021-01-17 22:56:09 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+require 'pry'
 def fizzbuzz(num)
   if num % 3 == 0 
     "Fizz"
@@ -7,6 +8,7 @@ def fizzbuzz(num)
     "Buzz"
   elsif num % (3.5) == 0
     "FizzBuzz"
+    binding.pry
   end
 end
 
